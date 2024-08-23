5168b48f5bf1166d05c57574aae3f2dbefe7a2e9
ethan-korl <ethenatx@gmail.com>
2024-08-23 17:56:56 +0000

    added backends and Survey_admin model

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,22 @@
 from inspect import classify_class_attrs
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.hashers import make_password
 
 
-class SurveyAdmin(models.Model):
+class BaseSurveyAdminManager:
+    # for creating admin
+    def _create_sa(self, username, password):
+        _harshed_password = make_password(password=password)
+        survey_admin =SurveyAdmin.objects.create(
+                                username=username, 
+                                password=_harshed_password
+                            )
+        survey_admin.save()
+
+
+
+class SurveyAdmin(models.Model, BaseSurveyAdminManager):
     """
     Survey admin are the only users with login accesss
     """
@@ -13,6 +26,9 @@ class SurveyAdmin(models.Model):
     is_active = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at  = models.DateTimeField(auto_now=True)
+    
+    def create_survey_admin(self, username, password):
+        return super()._create_sa(username, password)
 
 
 
