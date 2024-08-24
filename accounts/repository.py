5f930a5d4181f0457f0eb5cb162020e510371520
ethan-korl <ethenatx@gmail.com>
2024-08-24 09:02:25 +0000

    add respositires and moved auth_backend

---
+++ accounts/repository.py
@@ -0,0 +1,51 @@
+from accounts.models import SurveyAdmin
+from typing import Optional
+from django.db import models
+
+class SurveyAdminRepository:
+    """
+    Repository class for managing SurveyAdmin instances.
+    """
+    sa = SurveyAdmin
+    
+    @classmethod
+    def get_by_id(cls, admin_id: str) -> Optional[SurveyAdmin]:
+        try:
+            return cls.sa.objects.get(admin_id=admin_id)
+        except cls.sa.DoesNotExist:
+            return None
+
+    @classmethod
+    def get_by_username(cls, username: str) -> Optional[SurveyAdmin]:
+        try:
+            return cls.sa.objects.get(username=username)
+        except cls.sa.DoesNotExist:
+            return None
+
+    @classmethod
+    def create(cls, username: str, password: str) -> SurveyAdmin:
+        admin = cls.sa.objects.create(username=username, password=password)
+        admin.save()
+        return admin
+    
+
+    @classmethod
+    def update(cls, admin_id: str, **kwargs) -> Optional[SurveyAdmin]:
+        admin = SurveyAdminRepository.get_by_id(admin_id)
+        if admin:
+            for attr, value in kwargs.items():
+                setattr(admin, attr, value)
+            admin.save()
+        return admin
+
+
+    @classmethod
+    def delete(cls, admin_id: str) -> bool:
+        try:
+            admin = SurveyAdminRepository.get_by_id(admin_id)
+            if admin:
+                admin.delete()
+                return True
+        except Exception as e:
+            pass
+        return False
--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,15 @@
-from  main.views import sa_dashboard
+from  main.views import (sa_dashboard, 
+                         create_survey, 
+                         survey_detail, 
+                         survey_result,
+                         sp_dashboard,
+                         )
 from django.urls import path
 
 urlpatterns = [
-    path('sa-dashbaord/', sa_dashboard, name="sa_dashboard"),
+    path('sa-dashboard/', sa_dashboard, name="sa_dashboard"),
+    path('sp-dashboard/<url_id>/', sp_dashboard, name="sp-dashboard"),
+    path('create-survey/', create_survey, name="create-survey"),
+    path('survey-result/', survey_result, name="survey-result"),
+    path('survey-detail/', survey_detail, name="survey-detail"),
 ]
