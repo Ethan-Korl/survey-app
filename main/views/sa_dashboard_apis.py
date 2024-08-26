86d80a2ad148f7d8856f29b4da4ba96e85325295
ethan-korl <ethenatx@gmail.com>
2024-08-26 18:58:40 +0000

    built sa_questions creation api

--- main/views/sa_dashboard_apis.py
+++ main/views/sa_dashboard_apis.py
@@ -7,6 +7,7 @@ from main.serializers import (
     ListSurveySerializer,
     QuestionSerializer,
 )
+from utils import check_if_required
 from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
@@ -42,25 +43,39 @@ class DeleteSurveyView(DestroyAPIView):
 
 class CreateQuestionView(CreateAPIView):
     serializer_class = QuestionSerializer
+    parser_classes = [MultiPartParser]
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request: Request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         try:
             if serializer.is_valid(raise_exception=True):
-                serializer.save()
-                context = {"detail": "Question"}
+                survey = serializer.validated_data.get("survey")
+                type_of_response_required = serializer.validated_data.get(
+                    "type_of_response_required"
+                )
+                question = serializer.validated_data.get("question")
+                answer_required = request.data.get("answer_required")
+
+                ques_repo.create(
+                    survey=survey,
+                    question=question,
+                    type_of_response_required=type_of_response_required,
+                    answer_required=check_if_required(answer_required),
+                )
+
+                context = {"detail": "Question Added"}
                 return Response(data=context, status=status.HTTP_200_OK)
 
         except ValidationError as ve:
             context = {"detail": ve.default_detail}
-            print(context)
+            print(ve)
             return Response(data=context, status=status.HTTP_400_BAD_REQUEST)
 
         return super().post(request, *args, **kwargs)
 
 
 class CreateSurveyView(CreateAPIView):
-    pagination_class = [MultiPartParser]
+    parser_classes = [MultiPartParser]
     serializer_class = CreateSurveySerializer
     model_repo = SurveyRepository
     permission_classes = [IsAuthenticated]
